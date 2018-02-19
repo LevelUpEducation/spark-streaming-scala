c5d84b56c95a6aafb7c645ca0ee8c247a6419a48
Markus Braasch <markus.braasch@gmail.com>
2018-02-19 09:19:16 -0500

    Some cleanup.

--- src/main/scala/StructuredStreaming/solutions/WindowOperationSolution.scala
+++ src/main/scala/StructuredStreaming/solutions/WindowOperationSolution.scala
@@ -40,26 +40,19 @@ object WindowOperationSolution {
 
 		implicit val tweetDataEncoder = org.apache.spark.sql.Encoders.kryo[TweetData]
 
-		val query: Dataset[TweetData] = records.
-			//printSchema()
+		val query = records.
 			as[(BigInt, String, String, String, String, BigInt, String)].
-			map(r => TweetData(r._1, r._2, r._3, r._4, r._5, r._6, r._7))
-			//as[TweetData]
-
-		val query2 = query.
-			//toDF("id", "userName", "place", "replyToScreenName", "createdAt", "length", "firstHashtag").
+			map(r => TweetData(r._1, r._2, r._3, r._4, r._5, r._6, r._7)).
 			filter(_.replyToScreenName != null).
-			//printSchema()
 			map(t => (t.replyToScreenName, new Timestamp(t.createdAt.toLong), t.id,
 						if (t.firstHashtag == null) 0 else t.firstHashtag.length)).
 			toDF("replyToScreenName", "createdAt", "id", "hashtagLength").
 			withWatermark("createdAt", "3 minutes").
 			groupBy(window($"createdAt", "10 minutes", "30 seconds"), $"replyToScreenName").
-			agg(count("id")/*, max("hashtagLength")*/).
-			//orderBy($"count(id)".desc).
+			agg(count("id"), max("hashtagLength")).
 			writeStream.format("console").
 			queryName("exerciseOutput").start
 
-		query2.awaitTermination()
+		query.awaitTermination()
 	}
 }
